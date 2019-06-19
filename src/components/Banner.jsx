e75eab5923cf7cbfa2a1228c273a7b628bd57268
coo_coo1 <kaloianbch@gmail.com>
2019-06-19 16:57:49 +0100

    chore: Remove unused file

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,12 +1,11 @@
 import React, {Component} from 'react';
-import './Banner.css'
 import bannerImg from '../res/bannerImg.png'
 
 class Banner extends Component {
     render() {
         return (
-            <div className="d-flex justify-content-center" id='banner'>
-                <img id='bannerImg' src={bannerImg} alt="SpaceX Explore" width="500rem"/>
+            <div className='d-flex justify-content-center' id='banner'>
+                <img id='bannerImg' src={bannerImg} alt='SpaceX Explore' width='500rem'/>
             </div>
 
         );
