312c764202e5d8ea46d39dde83cc36a34b3903a7
coo_coo1 <kaloianbch@gmail.com>
2019-06-19 21:22:07 +0100

    fix: SearchBar Icon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import './SearchBar.css'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faSearch} from "@fortawesome/free-solid-svg-icons/faSearch";
 
 class SearchBar extends Component {
     render() {
@@ -7,7 +9,9 @@ class SearchBar extends Component {
             <div class='d-flex justify-content-center'>
                 <div class='input-group col-5'>
                 <input type='text' class='form-control' placeholder='Search..'/>
-                <button type="button" class="btn btn-light" id='searchButton'><i className="fa fa-search"></i></button>
+                <button type='button' className='btn btn-light' id='searchButton'>
+                    <FontAwesomeIcon icon={faSearch}/>
+                </button>
                 </div>
             </div>
         );
