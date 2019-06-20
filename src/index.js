71f18b2c846d99dde292bdd5c4a355ba9c56c0ab
coo_coo1 <kaloianbch@gmail.com>
2019-06-20 01:18:03 +0100

    chore: Change packages
    Reverted back to vanilla bootstrap + react-bootstrap

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import 'mdbootstrap/css/bootstrap.min.css'
-import 'mdbootstrap/css/mdb.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap/dist/react-bootstrap.min.js'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
