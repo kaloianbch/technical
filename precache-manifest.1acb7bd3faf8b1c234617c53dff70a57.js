ac4f745c784442687ff3daa5a8526a52c5f6f318
coo_coo1 <kaloianbch@gmail.com>
2019-06-21 07:32:32 +0100

    Updates

---
+++ precache-manifest.1acb7bd3faf8b1c234617c53dff70a57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efad5bb4983dab223cf15d4b422c8a21",
+    "url": "/technical/index.html"
+  },
+  {
+    "revision": "ee0986ff03852b06431d",
+    "url": "/technical/static/css/2.43d354e1.chunk.css"
+  },
+  {
+    "revision": "45c158d1b5e4594e720c",
+    "url": "/technical/static/css/main.d7604ba4.chunk.css"
+  },
+  {
+    "revision": "ee0986ff03852b06431d",
+    "url": "/technical/static/js/2.3779bd7d.chunk.js"
+  },
+  {
+    "revision": "45c158d1b5e4594e720c",
+    "url": "/technical/static/js/main.56d3a1e7.chunk.js"
+  },
+  {
+    "revision": "c3df6ffe88a43650f9ee",
+    "url": "/technical/static/js/runtime~main.0b28831a.js"
+  },
+  {
+    "revision": "81d40ec18204023ee4c2ecc4e40c7aa8",
+    "url": "/technical/static/media/bannerImg.81d40ec1.png"
+  }
+]);
